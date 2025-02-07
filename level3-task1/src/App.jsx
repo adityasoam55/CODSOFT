aebe50a4a5b03329440a187eac51e326c67e6759
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2025-02-07 21:40:25 +0530

    Login page added

--- level3-task1/src/App.jsx
+++ level3-task1/src/App.jsx
@@ -7,6 +7,7 @@ import AllProducts from './components/AllProducts'
 import CategoryList from './components/CategoryList'
 import CategoryProducts from './components/CategoryProducts'
 import ProductDetails from './components/ProductDetails'
+import Login from './components/Login'
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/productdetails/:id" element={<ProductDetails  />} />
         <Route path="/categorylist/" element={<CategoryList />} />
         <Route path="/category/:category" element={<CategoryProducts />} />
+        <Route path="/login/" element={<Login />} />
       </Routes>
       <Footer />
     </div>
---
+++ level3-task1/src/components/Login.jsx
@@ -0,0 +1,74 @@
+import { Form, Formik, useFormik } from "formik";
+import Input, { FormikInput } from "./Input";
+import React from "react";
+import * as Yup from 'yup';
+import { Link } from "react-router-dom";
+
+function Login() {
+
+    function callLoginAPI({email, password}) {
+        console.log("calling login API", email, password)
+    }
+
+    const loginSchema = Yup.object().shape({
+        email: Yup.string().email().required(),
+        password: Yup.string().min(8).max(12).required()
+    })
+
+    const initialValues = {
+        email: "",
+        password: ""
+    }
+
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-8 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-8 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+                <Formik
+                    onSubmit={callLoginAPI}
+                    validationSchema={loginSchema}
+                    initialValues={initialValues}
+                >
+                    <Form className="space-y-6">
+                        <FormikInput
+                            id="email"
+                            name="email"
+                            label="Email address"
+                            type="email"
+                        />
+
+                        <FormikInput
+                            id="password"
+                            name="password"
+                            label="Password"
+                            type="password"
+                        />
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-gray-400 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-xs hover:bg-gray-300 focus-visible:outline-2 "
+                            >
+                                Sign in
+                            </button>
+                        </div>
+                    </Form>
+                </Formik>
+
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+                    Don't have Account?{' '}
+                    <Link to="/signup/" className="font-semibold text-gray-900 hover:text-gray-700 hover:underline hover:underline-offset-4">
+                        SignUp here
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
